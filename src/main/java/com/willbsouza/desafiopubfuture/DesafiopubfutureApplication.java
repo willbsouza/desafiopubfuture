a112c3edb1bd0e69005d06197d739b1e3744f062
DE LIMA SOUZA <w.delimasouza@dxc.com>
2022-01-07 11:28:38 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/willbsouza/desafiopubfuture/DesafiopubfutureApplication.java
@@ -0,0 +1,13 @@
+package com.willbsouza.desafiopubfuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiopubfutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiopubfutureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willbsouza/desafiopubfuture/DesafiopubfutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willbsouza.desafiopubfuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiopubfutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
